95f7f41951d0af8c8d0568c7315c420be7d69c97
Niklas Furberg <niklas.furberg@dynabyte.se>
2023-06-18 21:58:08 +0200

    refactored to separate body, vel and 3dobject tuples

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import os
 import numpy as np
 
 
-Body = namedtuple('Body', 'pos vel mass')
+Body = namedtuple('Body', 'mass')
+Velocity = namedtuple('Velocity', 'lin rot')
+SpatialObject = namedtuple('SpatialObject', 'body pos vel')
 Force = namedtuple('Force', 'dir magnitude')
 
 def run():
     frame_count = 0
-    body = Body(np.zeros(3), np.zeros(3), 1.0)
+    body = Body(1.0)
+    vel = Velocity(lin=np.zeros(3), rot=np.zeros(3))
+    spatialObject = SpatialObject(body=body, pos=np.zeros(3), vel=vel)
     f1 = Force(np.array([0.0, 1.0, 0.0]), 4.0)
     start = time.time()
     time_passed = 0
@@ -23,15 +27,16 @@ def run():
         # os.system('cls')
         # print(body)
         # print(delta_time)
-        time.sleep(0.002)
-        acc = (f1.magnitude * f1.dir) / body.mass
-        vel = body.vel + (acc * delta_time)
-        pos = body.pos + (vel * delta_time)
-        body = Body(pos, vel, body.mass)
+        #time.sleep(0.002)
+        lin_acc = (f1.magnitude * f1.dir) / spatialObject.body.mass
+        lin_vel_ = spatialObject.vel.lin + (lin_acc * delta_time)
+        vel_ = Velocity(lin=lin_vel_, rot=spatialObject.vel.rot)
+        pos_ = spatialObject.pos + (lin_vel_ * delta_time)
+        spatialObject = SpatialObject(body, pos_, vel_)
 
         frame_count += 1
         time_passed = now - start
-    print(body)
+    print(spatialObject)
     print("time_passed: " + str(time_passed))
     print("fps: " + str(frame_count / 10.0))
 
