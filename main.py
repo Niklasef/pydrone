b56d6f24223dc7e831bc264fcbac95d796917574
Niklas Furberg <niklas.furberg@dynabyte.se>
2023-06-18 20:36:30 +0200

    uses numpy vectors, fixes so uses current velocity

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from collections import namedtuple
 import time
 import os
+import numpy as np
 
 
-Body = namedtuple('Body', 'x_pos y_pos z_pos x_vel y_vel z_vel mass')
-Force = namedtuple('Force', 'x y z magnitude')
+Body = namedtuple('Body', 'pos vel mass')
+Force = namedtuple('Force', 'dir magnitude')
 
 def run():
     frame_count = 0
-    body = Body(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0)
-    f1 = Force(0.0, 1.0, 0.0, 4.0)
+    body = Body(np.zeros(3), np.zeros(3), 1.0)
+    f1 = Force(np.array([0.0, 1.0, 0.0]), 4.0)
     start = time.time()
     time_passed = 0
     prev_frame = 0
@@ -23,16 +24,10 @@ def run():
         # print(body)
         # print(delta_time)
         time.sleep(0.002)
-        x_acc = (f1.magnitude * f1.x) / body.mass
-        y_acc = (f1.magnitude * f1.y) / body.mass
-        z_acc = (f1.magnitude * f1.z) / body.mass
-        x_vel = body.x_vel + (x_acc * delta_time)
-        y_vel = body.y_vel + (y_acc * delta_time)
-        z_vel = body.z_vel + (z_acc * delta_time)
-        x_pos = body.x_pos + (body.x_vel * delta_time)
-        y_pos = body.y_pos + (body.y_vel * delta_time)
-        z_pos = body.z_pos + (body.z_vel * delta_time)
-        body = Body(x_pos, y_pos, z_pos, x_vel, y_vel, z_vel, body.mass)
+        acc = (f1.magnitude * f1.dir) / body.mass
+        vel = body.vel + (acc * delta_time)
+        pos = body.pos + (vel * delta_time)
+        body = Body(pos, vel, body.mass)
 
         frame_count += 1
         time_passed = now - start
