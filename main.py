d947e074750a10cd130306ba65db5362be8c9c3e
Niklas Furberg <niklas.furberg@dynabyte.se>
2023-06-26 22:22:19 +0200

    Corner Line Rotation Force

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyrr import Matrix44, matrix44, Vector3
 Body = namedtuple('Body', 'mass')
 Velocity = namedtuple('Velocity', 'lin rot')
 SpatialObject = namedtuple('SpatialObject', 'body pos vel')
-Force = namedtuple('Force', 'dir magnitude')
+Force = namedtuple('Force', 'dir pos magnitude')
 
 
 # Initialize FPS counter variables
@@ -39,10 +39,13 @@ indices = np.array([
 
 def run():
     frame_count = 0
-    body = Body(1.0)
+    body = Body(mass=1.0)
     vel = Velocity(lin=np.zeros(3), rot=np.zeros(3))
     spatialObject = SpatialObject(body=body, pos=np.zeros(3), vel=vel)
-    f1 = Force(np.array([0.0, 1.0, 0.0]), 2.0)
+    f1 = Force(
+        dir=np.array([0.0, 1.0, 0.0]),
+        pos=np.array([-0.5, 0.0, 0.5]),
+        magnitude=0.2)
     start = time.time()
     time_passed = 0
     prev_frame = 0
@@ -50,12 +53,11 @@ def run():
 
     window, shader, VAO = init(vertices, indices)
     translation = Matrix44.from_translation(Vector3([0.0, 0.0, 0.0]))
-    rotation_angle = 0.0
-    rotation_x = Matrix44.from_x_rotation(np.radians(rotation_angle))  # Rotate by rotation_angle degrees around x-axis
-    rotation_y = Matrix44.from_y_rotation(np.radians(rotation_angle))  # Rotate by rotation_angle degrees around y-axis
-    rotation = matrix44.multiply(rotation_x, rotation_y)  # Combine the two rotations    
+    rotation = Matrix44.identity()
 
-    while window_active(window):
+    inertia = spatialObject.body.mass * (1.0 * 1.0) * (3.0/10.0) #  when force at corner and acting perpendicular to face
+    # while window_active(window):
+    while time_passed < 10.0:
         now = time.time()
         delta_time = now - (prev_frame if prev_frame != 0 else now)
         prev_frame = now
@@ -64,16 +66,33 @@ def run():
         # print(spatialObject)
         # print(delta_time)
         #time.sleep(0.002)
-        lin_acc = (f1.magnitude * f1.dir) / spatialObject.body.mass
+
+        rotated_dir = matrix44.apply_to_vector(rotation, f1.dir)
+        rotated_pos = matrix44.apply_to_vector(rotation, f1.pos)
+        f1_ = Force(dir=rotated_dir, pos=rotated_pos, magnitude=f1.magnitude)
+
+        lin_acc = (f1_.magnitude * f1_.dir) / spatialObject.body.mass
         lin_vel_ = spatialObject.vel.lin + (lin_acc * delta_time)
-        vel_ = Velocity(lin=lin_vel_, rot=spatialObject.vel.rot)
         pos_ = spatialObject.pos + (lin_vel_ * delta_time)
-        spatialObject = SpatialObject(body, pos_, vel_)
 
-        translation = Matrix44.from_translation(Vector3(spatialObject.pos))
+        tourque = np.cross(f1_.pos, f1_.dir * f1_.magnitude)
+        rot_acc = tourque / inertia
+        rot_vel_ = spatialObject.vel.rot + (rot_acc * delta_time)
+        rot_speed = np.linalg.norm(spatialObject.vel.rot)
+        rot_axis = spatialObject.vel.rot / rot_speed if rot_speed > 0 else np.array([1.0, 0.0, 0.0])
+        rot_angle = rot_speed * delta_time
 
-        render(window, shader, VAO, indices, -15, -50, rotation, translation)
 
+        translation = Matrix44.from_translation(Vector3(pos_))
+        rotation_ = matrix44.create_from_axis_rotation(rot_axis, rot_angle)
+        rotation = matrix44.multiply(rotation, rotation_)
+
+        render(window, shader, VAO, indices, 0, -5, rotation, translation)
+
+        spatialObject = SpatialObject(
+            spatialObject.body,
+            pos_,
+            Velocity(lin=lin_vel_, rot=rot_vel_))
         frame_count += 1
         time_passed = now - start
     print(spatialObject)
