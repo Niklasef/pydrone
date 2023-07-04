fac1a8574a6ad743a478e10e93316d7566d4da13
Niklas Furberg <niklas.furberg@dynabyte.se>
2023-07-04 23:02:48 +0200

    clarifyed param names

--- Physics.py
+++ Physics.py
@@ -6,9 +6,9 @@ Body = namedtuple('Body', 'mass')
 Force = namedtuple('Force', 'dir pos magnitude')
 Velocity = namedtuple('Velocity', 'lin rot')
 
-def apply_rot_force(forces, rot_vel, time, body):
+def apply_rot_force(local_forces, rot_vel, time, body):
     inertia = body.mass * (1.0 * 1.0) * (3.0/10.0) #  when force at corner and acting perpendicular to face
-    total_torque = sum((np.cross(force.pos, force.dir * force.magnitude) for force in forces), start=np.array([0.0, 0.0, 0.0]))
+    total_torque = sum((np.cross(force.pos, force.dir * force.magnitude) for force in local_forces), start=np.array([0.0, 0.0, 0.0]))
     rot_acc = total_torque / inertia
 
     rot_vel_delta = rot_acc * time
@@ -20,8 +20,8 @@ def apply_rot_force(forces, rot_vel, time, body):
 
     return rot_axis, rot_angle, rot_vel_
 
-def apply_trans_force(forces, lin_vel, time, body):
-    total_force = sum((force.magnitude * force.dir for force in forces), start=0)
+def apply_trans_force(local_forces, lin_vel, time, body):
+    total_force = sum((force.magnitude * force.dir for force in local_forces), start=0)
     lin_acc = total_force / body.mass
     lin_vel_delta = lin_acc * time
     lin_vel_ = lin_vel + lin_vel_delta
